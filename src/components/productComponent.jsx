9595082def7f2dd97ef19676635c7c18db1069d1
Ismael Soares <ismaelsoaresdoria@gmail.com>
2016-05-12 13:19:01 -0300

    Add productComponent

---
+++ src/components/productComponent.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+let ProductCategoryRow = React.createClass({
+  render(){
+    return (
+      <tr><th colSpan="2">{this.props.category}</th></tr>
+    );
+  }
+});
+
+let ProductRow = React.createClass({
+  render() {
+    let name = this.props.product.stocked ?
+      this.props.product.name :
+      <span style={{color: 'red'}}>
+        {this.props.product.name}
+      </span>;
+    return (
+      <tr>
+        <td>{name}</td>
+        <td>{this.props.product.price}</td>
+      </tr>
+    );
+  }
+});
+
+let ProductTable = React.createClass({
+  render() {
+    let rows = [];
+    let lastCategory = null;
+    this.props.products.forEach(function(product) {
+      if (product.category !== lastCategory) {
+        rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
+      }
+      rows.push(<ProductRow product={product} key={product.name} />);
+      lastCategory = product.category;
+    });
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  }
+});
+
+let SearchBar = React.createClass({
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="Search..." />
+        <p>
+          <input type="checkbox" />
+          {' '}
+          Only show products in stock
+        </p>
+      </form>
+    );
+  }
+});
+
+let FilterableProductTable = React.createClass({
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <ProductTable products={this.props.products} />
+      </div>
+    );
+  }
+});
+
+
+let PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
+
+
+//ReactDOM.render(<FilterableProductTable products={PRODUCTS} />,document.getElementById('container'));
+export default FilterableProductTable;
